c38f6e7442f626158435537a8810ff1a2dd6bbb1
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2022-09-01 20:33:47 -0300

    Create cart.daos.ts

---
+++ src/daos/cart.daos.ts
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+import { CartModel } from "../models/carts.model";
+import { CartDto } from "../dtos/cart.dto";
+import { ProductoDto } from "../dtos/products.dto";
+
+
+export class CartDao {
+    constructor() {}
+    
+    async getCarts() {
+        try {
+            return await CartModel.find();
+        } catch (e) {
+            return e;
+        }
+    }
+
+    async getCartById(cartId: string) {
+        try {
+            return await CartModel.findById(cartId);
+        } catch (e) {
+            return e;
+        }
+    }
+
+    async getCartByEmail(email: string) {
+        try {
+            return await CartModel.findOne({ email });
+        } catch (e) {
+            return e;
+        }
+    }
+
+    async createCart(email: string, product: ProductoDto) {
+        try {
+            const data = new CartModel({ email, productos: product })
+            return await data.save()
+        } catch (e) {
+            return e;
+        }
+    }
+
+    async updateCartById(cartId: string, product: ProductoDto) {
+        try {
+            return  await CartModel.findByIdAndUpdate({_id: cartId}, {$push: {productos: product}})
+        } catch (e) {
+            return e;
+        }
+    }
+
+    async deleteProductCartById(cartId: string, product: ProductoDto) {
+        try {
+            return await CartModel.findByIdAndDelete({_id: cartId}, {$pull: {productos: product}})
+        } catch (e) {
+            return e;
+        }
+    }
+
+    async deleteCartById(cartId: string) {
+        try {
+            return await CartModel.findByIdAndDelete({_id: cartId})
+        } catch (e) {
+            return e;
+        }
+    }
+}

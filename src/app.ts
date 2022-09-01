afce511e4ec0a7af0ff7d35a7863c864c928c874
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2022-09-01 20:22:56 -0300

    Create app.ts

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import { config } from './config/config';
+import { routerProducts } from './routes/products.route'
+import { routerAuth } from './routes/auth.route';
+import dbConnect from './database/db';
+import { routerCart } from './routes/cart.route';
+
+
+dbConnect().then()
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+
+app.use('/api/products', routerProducts)
+app.use('/api/auth', routerAuth)
+app.use('/api/carts', routerCart)
+
+const server = app.listen(config.PORT, () => 
+    console.log(`Servidor corriendo en http://localhost:${config.PORT}`)
+)
+server.on("error", (error) => 
+    console.log("Error en el servidor: ", error)
+    );

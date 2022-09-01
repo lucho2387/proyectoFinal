ee30c22f0e56f0c586f3ca3f23271ddba41dd230
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2022-09-01 20:30:15 -0300

    Create products.interface.ts

---
+++ src/interfaces/products.interface.ts
@@ -0,0 +1,10 @@
+export interface IProducto {
+    id?: number;
+    nombre: string;
+    descripcion: string;
+    categoria: string;
+    codigo: Number
+    imagen: string;
+    precio: Number;
+    stock: Number;
+}

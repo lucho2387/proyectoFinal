c276b5afac26dc1f71037b789852eb329e37dc73
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2022-09-01 20:25:36 -0300

    Delete info.routes.js

--- src/routes/info.routes.js
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express';
-import * as infoCtrl from '../controllers/info.controller'
-
-
-const router = Router();
-
-router.get('/', infoCtrl.getInfo)
-
-export default router;

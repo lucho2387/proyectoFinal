02b27e14ed4e2f162352dcb027b894b34f2c2ad4
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2022-09-02 12:09:51 -0300

    Update products.route.ts

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -1,21 +1,22 @@
 import { Router } from "express";
 import { ProductosController } from "../controllers/products.controller";
 import { isAdmin } from "../middlewares/isAdmin";
+import { checkJwt } from "../middlewares/session";
 
 const routerProducts = Router()
 const controller = new ProductosController();
 
 
-routerProducts.post("/", isAdmin, controller.createProduct);
+routerProducts.post("/", isAdmin, checkJwt, controller.createProduct);
 
 routerProducts.get("/", controller.getProducts);
 
 routerProducts.get('/:productId', controller.getProductById)
 
-routerProducts.put("/:productId", isAdmin, controller.updateProduct);
+routerProducts.put("/:productId", isAdmin, checkJwt, controller.updateProduct);
 
-routerProducts.get('/:categoria/productos', controller.getProductsCategory)
+routerProducts.get('/:categoria/productos', checkJwt,controller.getProductsCategory)
 
-routerProducts.delete('/:productId', isAdmin, controller.deleteProductById)
+routerProducts.delete('/:productId', isAdmin, checkJwt, controller.deleteProductById)
 
 export { routerProducts }

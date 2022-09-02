df800fcc45a2d1e13a1d0ba6b9a94bd46ddba0f1
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2022-09-01 21:57:29 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import { routerProducts } from './routes/products.route'
 import { routerAuth } from './routes/auth.route';
 import dbConnect from './database/db';
 import { routerCart } from './routes/cart.route';
+import { routerOrders } from './routes/orders.route';
 
 
 dbConnect().then()
@@ -18,6 +19,7 @@ app.use(cors())
 app.use('/api/products', routerProducts)
 app.use('/api/auth', routerAuth)
 app.use('/api/carts', routerCart)
+app.use('/api/orders', routerOrders)
 
 const server = app.listen(config.PORT, () => 
     console.log(`Servidor corriendo en http://localhost:${config.PORT}`)

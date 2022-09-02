9ed4b02251beb2f9035fce1cd3fa596a280e0186
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2022-09-01 23:06:01 -0300

    Create isAdmin.ts

---
+++ src/middlewares/isAdmin.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express"
+import { config } from "../config/config"
+
+export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        if(config.ADMIN === "ADMIN") {
+            return next()
+        }
+        res.json({mensaje: "No esta autorizado para acceder a la ruta."})
+    } catch (error) {
+        res.status(500).json({error: error})
+    }
+}

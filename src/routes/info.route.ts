0bd3475f57b167c7d8dfd863a893488a04dddcef
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2022-09-02 12:10:30 -0300

    Update info.route.ts

--- src/routes/info.route.ts
+++ src/routes/info.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { InfoController } from "../controllers/info.controller";
+import { isAdmin } from "../middlewares/isAdmin";
+import { checkJwt } from "../middlewares/session";
 
 const routerInfo  = Router();
 const controller = new InfoController();
 
-routerInfo.get("/", controller.getInfo);
+routerInfo.get("/", isAdmin, checkJwt, controller.getInfo);
 
 
 export { routerInfo };

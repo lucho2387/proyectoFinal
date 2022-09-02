d7818e57a2b73290c5ecb7ca1cf20b53d8f0da89
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2022-09-01 22:00:00 -0300

    Create orders.service.ts

---
+++ src/services/orders.service.ts
@@ -0,0 +1,43 @@
+// import { PreMiddlewareFunction } from "mongoose";
+import { CartDao } from "../daos/cart.daos";
+import { OrderDao } from "../daos/orders.daos";
+
+import { CartDto } from "../dtos/cart.dto";
+import { OrderDto } from "../dtos/orders.dto";
+import { ProductoDto } from "../dtos/products.dto";
+// import { config, logger } from "../utils";
+
+const cartDao = new CartDao();
+const orderDao = new OrderDao();
+
+export class OrdersService {
+    async createOrder(email: string): Promise<OrderDto | undefined> {
+        try {
+            const cart = await cartDao.getCartByEmail(email);
+            const d = new Date();
+            const fyh = `${d.getDay()}-${
+                d.getMonth() + 1
+            }-${d.getFullYear()} --- ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+            let ordenId = 1;
+            const orders = await orderDao.getAll();
+            if (orders && orders.length >= 1) {
+                ordenId = orders[orders.length - 1].ordenId + 1;
+            }
+            const orden: OrderDto = new OrderDto({
+                email: email,
+                productos: cart.productos,
+                fyh: fyh,
+                ordenId: ordenId,
+                estado: "GENERADA",
+            });
+            const ordenResp = await orderDao.createOrder(orden);
+            if (ordenResp) {
+                const cartId = cart._id
+                await cartDao.deleteCartById(cartId);
+                return ordenResp;
+            } else return undefined;
+        } catch (e) {
+            return e;
+        }
+    }
+}

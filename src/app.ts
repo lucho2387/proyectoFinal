3f46972a7a8690c7ab1d86e1251fabdc2c033c26
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2022-09-02 11:45:12 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -7,23 +7,33 @@ import { routerAuth } from './routes/auth.route';
 import dbConnect from './database/db';
 import { routerCart } from './routes/cart.route';
 import { routerOrders } from './routes/orders.route';
-
+import path from 'path'
+import { routerInfo } from './routes/info.route';
 
 dbConnect().then()
 const app = express()
 
+// Configuracion
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug');
+
+// Middlwares
 app.use(express.json())
 app.use(cors())
 
-
+// Routes
 app.use('/api/products', routerProducts)
 app.use('/api/auth', routerAuth)
 app.use('/api/carts', routerCart)
 app.use('/api/orders', routerOrders)
+app.use('/api/info', routerInfo)
+
+//Static Files
+app.use(express.static(path.join(__dirname, 'public')));
 
 const server = app.listen(config.PORT, () => 
     console.log(`Servidor corriendo en http://localhost:${config.PORT}`)
 )
 server.on("error", (error) => 
     console.log("Error en el servidor: ", error)
-    );
+);

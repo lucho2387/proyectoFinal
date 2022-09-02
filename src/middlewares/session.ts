33abb8c0c22ca620dba91771c07ab1af98c15dec
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2022-09-02 14:15:15 -0300

    Update session.ts

--- src/middlewares/session.ts
+++ src/middlewares/session.ts
@@ -8,19 +8,16 @@ const checkJwt = (req: RequestExt, res: Response, next: NextFunction) => {
     const jwtByUser = req.headers.authorization || null
     const jwt = jwtByUser.split(" ").pop()
     const user = verifyToken(`${jwt}`) as { id: string };
-    console.log({user});
     if (!user) {
       res.status(401)
       res.send("El JWT no es valido")
     } else {
-        console.log({jwtByUser});
         req.user = user
         next()
     }
   } catch (e) {
-    console.log({ e })
     res.status(400)
-    res.json({ mensjae: "Session no Valida" })
+    res.json({ mensaje: "El JWT no es valido" })
   }
 };
 

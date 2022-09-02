54133599b7c7b08092c8edb8da334f77d0a30b24
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2022-09-02 12:10:48 -0300

    Update cart.route.ts

--- src/routes/cart.route.ts
+++ src/routes/cart.route.ts
@@ -1,20 +1,21 @@
 import { Router } from "express";
 import { CartController } from "../controllers/cart.controller";
 import { isAdmin } from "../middlewares/isAdmin";
+import { checkJwt } from "../middlewares/session";
 
 const routerCart = Router();
 const controller = new CartController();
 
 routerCart.get('/', isAdmin, controller.getCarts)
 
-routerCart.get('/:cartId', isAdmin,controller.getCartById)
+routerCart.get('/:cartId', isAdmin, controller.getCartById)
 
-routerCart.get('/users/:email', controller.getCartByEmail)
+routerCart.get('/users/:email', checkJwt, controller.getCartByEmail)
 
-routerCart.post('/:productId', controller.createCart)
+routerCart.post('/:productId', checkJwt, controller.createCart)
 
-routerCart.delete('/:cartId/productos/:productId', controller.deleteProductCartById)
+routerCart.delete('/:cartId/productos/:productId', checkJwt, controller.deleteProductCartById)
 
-routerCart.delete('/:cartId', isAdmin,controller.deleteCartById)
+routerCart.delete('/:cartId', isAdmin, checkJwt, controller.deleteCartById)
 
 export { routerCart };

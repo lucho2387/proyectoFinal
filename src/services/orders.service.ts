d9dc1ff9efdaee0fadcd115ccb303bf73c803258
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2022-09-04 10:12:03 -0300

    Update orders.service.ts

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -1,3 +1,4 @@
+import moment from "moment";
 import { CartDao } from "../daos/cart.daos";
 import { OrderDao } from "../daos/orders.daos";
 import { OrderDto } from "../dtos/orders.dto";
@@ -10,9 +11,7 @@ export class OrdersService {
         try {
             const cart = await cartDao.getCartByEmail(email);
             const d = new Date();
-            const fyh = `${d.getDay()}-${
-                d.getMonth() + 1
-            }-${d.getFullYear()} --- ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+            const fecha = moment().format("DD/MM/YYYY --- HH:mm:ss A")
             let ordenId = 1;
             const orders = await orderDao.getAll();
             if (orders && orders.length >= 1) {
@@ -21,7 +20,7 @@ export class OrdersService {
             const orden: OrderDto = new OrderDto({
                 email: email,
                 productos: cart.productos,
-                fyh: fyh,
+                fyh: fecha,
                 ordenId: ordenId,
                 estado: "GENERADA",
             });
@@ -30,17 +29,26 @@ export class OrdersService {
                 const cartId = cart._id
                 await cartDao.deleteCartById(cartId);
                 return ordenResp;
-            } else return undefined;
+            }
         } catch (e) {
             return e;
         }
     }
     
-      async getOrders(email: string) {
+     async getOrdersEmail(email: string) {
         try {
-            return await orderDao.getByEmail(email);
+            return await orderDao.getOrdersEmail(email);
         } catch (e) {
             return e;
         }
     }
+    
+    async getOrders() {
+        try {
+            return await orderDao.getOrders();
+        } catch (e) {
+            return e;
+        }
+    }
+    
 }

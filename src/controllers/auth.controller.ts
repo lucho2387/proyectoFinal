37cfcae7a01fdc67267fecec13f545ba85c1c38b
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2022-09-05 18:39:49 -0300

    Update auth.controller.ts

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,13 +2,21 @@ import { Response, Request } from "express"
 import { registerNewUser, loginUser } from "../services/auth.service"
 
 const registerCtrl = async (req: Request, res: Response) => {
-    const data = req.body
-    const responseUser = await registerNewUser(data)
-    res.json({
-        usuario: responseUser,
-    })
+    const { nombre, apellido, telefono, direccion, email, password, confirmPassword } = req.body
+    
+    if(password !== confirmPassword){
+        res.json({mensaje: 'Las contraseñas no coiciden'})
+    }else {
+        if(password.length < 4){
+            res.json({mensaje: 'La contraseña debe contener mas de 4 caracteres'})
+        }else {
+            const responseUser = await registerNewUser({nombre, apellido, telefono, direccion, email, password})
+            res.json({
+                usuario: responseUser,
+            })
+        }
+    }
 }
-
 
 const loginCtrl = async (req: Request, res: Response) => {
     const { email, password } = req.body

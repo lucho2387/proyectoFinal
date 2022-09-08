def18ed75afcfe3dbc4e680fa896ed007cad9a32
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2022-09-08 10:05:05 -0300

    Update carts.model.ts

--- src/models/carts.model.ts
+++ src/models/carts.model.ts
@@ -3,37 +3,18 @@ import { ICart } from "../interfaces/carts.interface";
 
 const CartSchema = new Schema<ICart>(
     {
-        // email: { type: String, required: true },
-        // productos: [
-        //     {
-        //         producto: {
-        //             nombre: String,
-        //             descripcion: String,
-        //             id: String,
-        //             categoria: String,
-        //             precio: Number,
-        //             thumbnail: String,
-        //             timestamp: Number,
-        //         },
-        //         cantidad: { type: Number },
-        //     },
-        // ],
-        // timestamp: { type: String, required: true },
         email: { type: String, required: true },
-        productos: [
-            {
-                type: Object,
-                // cantidad: {
-                //     type: Number,
-                //     require: true,
-                // }
-            },
-            
-        ],
-        // productos: [{
-        //     ref: "productos",
-        //     type: Schema.Types.ObjectId
-        // }]
+         productos: [{
+            _id: false,
+            id: String,
+            nombre: String,
+            precio: Number,
+            cantidad: {
+                type: Number,
+                required: true,
+                default: 1
+            }
+        }],
     },
     {
         timestamps: true,
